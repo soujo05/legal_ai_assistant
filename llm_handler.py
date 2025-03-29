8bf8f53a30348752c58df2145c558924ec98155d
SOUJANYA ROY <146373786+soujo05@users.noreply.github.com>
2025-03-30 01:08:26 +0530

    Update llm_handler.py

--- llm_handler.py
+++ llm_handler.py
@@ -1,8 +1,8 @@
 import requests
 from retrieval import setup_rag
 
-GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
-API_KEY = "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"
+GROQ_API_URL = os.getenv("GROQ_API_URL")
+API_KEY = os.getenv("API_KEY")
 
 def ask_legal_ai(query):
     temp_retriever, global_retriever = setup_rag()
